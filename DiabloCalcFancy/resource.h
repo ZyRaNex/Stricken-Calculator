530fa45ea3a0386670fbc2be3796e91abcc86167
ZyRaNex <eeggaall@googlemail.com>
2018-09-18 06:16:05 +0200

    Add files via upload

---
+++ DiabloCalcFancy/resource.h
@@ -0,0 +1,57 @@
+//{{NO_DEPENDENCIES}}
+// Von Microsoft Visual C++ generierte Includedatei.
+// Verwendet durch DiabloCalcFancy.rc
+//
+#define IDD_DIALOG1                     101
+#define IDC_CHECK1                      1001
+#define IDC_POWER                       1001
+#define IDC_CHECK2                      1002
+#define IDC_POWER2                      1002
+#define IDC_CHANNEL                     1002
+#define IDC_BUTTON1                     1003
+#define IDC_EDIT1                       1004
+#define IDC_OUTA1                       1005
+#define IDC_OUTB1                       1006
+#define IDC_OUTA3                       1007
+#define IDC_OUTA4                       1008
+#define IDC_OUTA5                       1009
+#define IDC_OUTA6                       1010
+#define IDC_OUTA2                       1011
+#define IDC_OUTB2                       1012
+#define IDC_OUTB3                       1013
+#define IDC_OUTB4                       1014
+#define IDC_OUTB5                       1015
+#define IDC_EDIT13                      1016
+#define IDC_OUTB6                       1016
+#define IDC_BossHP                      1017
+#define IDC_SheetDamage                 1018
+#define IDC_CritDamage                  1019
+#define IDC_CritChance                  1020
+#define IDC_StrickenLevel               1021
+#define IDC_TrappedLevel                1022
+#define IDC_ZeisLevel                   1023
+#define IDC_SPEED                       1024
+#define IDC_SWIFTHARVEST                1025
+#define IDC_RiftLevel                   1026
+#define IDC_cooldown                    1027
+#define IDC_C2ooldown                   1027
+#define IDC_EleDamage                   1028
+#define IDC_CorpseDamage                1029
+#define IDC_TragoulDamage               1030
+#define IDC_EssenceBonus                1031
+#define IDC_STRONGARMS                  1032
+#define IDC_EliteDamage                 1033
+#define IDC_KrysbinDamage               1034
+#define IDC_OUTC                        1035
+#define IDC_Life                        1036
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
